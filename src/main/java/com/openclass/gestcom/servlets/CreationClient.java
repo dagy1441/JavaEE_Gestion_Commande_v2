3416b34761e1d27c9502b37d08222c2128e4f3f0
Dagnogo Yaya <dagy1441@gmail.com>
2019-02-19 14:54:47 +0000

    servlet created

--- src/main/java/com/openclass/gestcom/servlets/CreationClient.java
+++ src/main/java/com/openclass/gestcom/servlets/CreationClient.java
@@ -5,6 +5,7 @@
  */
 package com.openclass.gestcom.servlets;
 
+import com.openclass.gestcom.beans.Client;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -17,69 +18,80 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author dagy
  */
-@WebServlet(name = "CreationClient", urlPatterns = {"/CreationClient"})
 public class CreationClient extends HttpServlet {
-
+/** Declaration des constantes**/
+    public static final String CHAMP_NOM              = "nomClient";
+    public static final String CHAMP_PRENOM           = "prenomClient";
+    public static final String CHAMP_ADRESSE          = "adresseClient";
+    public static final String CHAMP_TELEPHONE        = "telephoneClient";
+    public static final String CHAMP_EMAIL            = "emailClient";
+    
+    public static final String ATTRIB_CLIENT          = "client";
+    public static final String ATTRIB_MESSAGE         = "message";
+    public static final String ATTRIB_ERREUR          = "erreur";
+    
+    public static final String VUE                    = "/afficherClient.jsp";
+       
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CreationClient</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CreationClient at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+     * Redefinition de la methode doGet
+     * @param request, 
+     * @param response,
+     **/
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+     /**Recuperation des donnees saisies, envoyees en tant que parametres de 
+      * la requete GET generee a la validation du formulaire
+      **/
+     String nom = request.getParameter(CHAMP_NOM);
+     String prenom = request.getParameter(CHAMP_PRENOM);
+     String adresse = request.getParameter(CHAMP_ADRESSE);
+     String telephone = request.getParameter(CHAMP_TELEPHONE);
+     String email = request.getParameter(CHAMP_EMAIL);
+     
+     String message;
+     boolean erreur;
+     
+     /** 
+      * Initialisation du message a afficher : si un des champs obligatoire
+      * du formulaire n'est pas renseigne, alors on affiche un message d'erreur,
+      * sinon on affiche un message de succes
+      **/
+        if (nom.trim().isEmpty() || prenom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() || email.trim().isEmpty()) {
+            message = "Erreur vous n'avez pas rempli tous les champs obligatoires.<br><a href=\"creerClient.jsp\">"
+                    + "Cliquez ici</a> pour acceder au formulaire de creation d'un client.";
+            erreur = true;
+        } else {
+            message = "Client cree avec succes !";
+            erreur = false;
+        }
+     
+        /**Creation du bean client et initialisation avec les donnees recuperees**/
+        Client client = new Client();
+        client.setNom(nom);
+        client.setPrenom(prenom);
+        client.setAdresse(adresse);
+        client.setTelephone(telephone);
+        client.setEmail(email);
+        
+        /**Ajout du bean Client et du message a l'objet request**/
+        request.setAttribute(ATTRIB_CLIENT, client);
+        request.setAttribute(ATTRIB_MESSAGE, message);
+        request.setAttribute(ATTRIB_ERREUR, erreur);
+        
+        /**Transmission a la page JSP en charge de l'affichage des donnees**/
+        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+    
+    
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+  
     @Override
     public String getServletInfo() {
         return "Short description";
